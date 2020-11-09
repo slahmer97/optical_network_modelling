612fb3d19d3e183e7afb79a4ef981fbb3ee2659a
slahmer <sidahmedlahmer@gmail.com>
2020-11-09 23:00:40 +0100

    dfgdfg

--- pred_done.py
+++ pred_done.py
@@ -14,6 +14,60 @@ def create_chunks(list_name, n):
         yield list_name[i:i + n]
 
 
+def process_data(X_train, Y_train=None):
+    X_P30 = []
+    MOD1 = []
+    MOD2 = []
+    MOD3 = []
+    MOD4 = []
+    MOD5 = []
+    MOD6 = []
+    MOD7 = []
+    MOD8 = []
+    Y_P30 = []
+    for i in range(0, len(X_train)):
+        example_train = X_train[i]
+        applied_modules = example_train[0]
+        p_in_32 = example_train[1]
+        if np.all(p_in_32 == 0):
+            continue
+        length = len(applied_modules)
+        X_P30.append(np.array(p_in_32).reshape((1, 32)))
+        if Y_P30:
+            Y_P30.append(np.array(Y_train[i]).reshape((1, 32)))
+        for k in range(0, 8):
+            if k < length:
+                mod = applied_modules[k]
+                if mod[0] == 'EDFA':
+                    mod_id = 1
+                else:
+                    mod_id = -1
+                param1 = mod[1][0]
+                param2 = mod[1][1]
+            else:
+                mod_id = 0
+                param1 = 0
+                param2 = 0
+            tmp = np.array([mod_id, param1, param2]).reshape((1, 3))
+            if k == 0:
+                MOD1.append(tmp)
+            elif k == 1:
+                MOD2.append(tmp)
+            elif k == 2:
+                MOD3.append(tmp)
+            elif k == 3:
+                MOD4.append(tmp)
+            elif k == 4:
+                MOD5.append(tmp)
+            elif k == 5:
+                MOD6.append(tmp)
+            elif k == 6:
+                MOD7.append(tmp)
+            elif k == 7:
+                MOD8.append(tmp)
+    return X_P30, MOD1, MOD2, MOD3, MOD4, MOD5, MOD6, MOD7, MOD8, Y_P30
+
+
 class Regressor:
     def __init__(self):
         import numpy as np
@@ -21,8 +75,8 @@ class Regressor:
         self.last_ac = None
         self.last_vac = None
         self.saved_weights = {}
-        self.batch_size = 1
-        self.epochs_num = 5
+        self.batch_size = 128
+        self.epochs_num = 20
         self.vector_input1 = keras.Input(shape=(32,), name="R30_input_1")
 
         self.hidden_left_0 = layers.Dense(128, name="hidden_left_0", activation="tanh")(self.vector_input1)
@@ -91,235 +145,72 @@ class Regressor:
                     self.params_input6, self.params_input7, self.params_input8],
             outputs=[self.output_layer],
         )
-
-        self.model.compile()
+        opt = tf.optimizers.Adam(lr=1e-3, decay=1e-3 / 200)
+        rms = tf.keras.metrics.RootMeanSquaredError()
+        mse = tf.keras.losses.MeanSquaredError()
+        self.model.compile(loss=mse, optimizer=opt, metrics=[rms])
         self.model.summary()
         keras.utils.plot_model(self.model, "my_model.png", show_shapes=True)
 
-    # Original X_train, Y_train are passed directly here
-    def enable_layers(self):
-        for layer_name in self.saved_weights:
-            self.model.get_layer(layer_name).Trainable = True
-            self.model.get_layer(layer_name).set_weights(self.saved_weights[layer_name])
-        self.saved_weights = {}
-
-    def disable_layers(self, start, stop=9):
-        for layer in range(start, stop):
-            # print("disable from {}/8".format(layer))
-
-            # input_param_layer_name = "module_params{}".format(layer + 1)
-            hidden_param_layer_name = "hidden_params{}".format(layer)
-
-            self.model.get_layer(hidden_param_layer_name).Trainable = False
-            tmp = self.model.get_layer(hidden_param_layer_name).get_weights()
-            self.saved_weights[hidden_param_layer_name] = tmp
-            shape1 = tmp[0].shape
-            shape2 = tmp[1].shape
-            self.model.get_layer(hidden_param_layer_name).set_weights([np.zeros(shape1), np.zeros(shape2)])
-
-    def sub_pred(self, X):
-        ret = []
-        applied_modules = X[0]
-        p_in_32 = X[1]
-        ret.append(np.array(p_in_32).reshape((1, 32)))
-        for i in range(0, 8):
-            if i < len(applied_modules):
-                mod = applied_modules[i]
-                if mod[0] == 'EDFA':
-                    mod_id = 1
-                else:
-                    mod_id = -1
-                param1 = mod[1][0]
-                param2 = mod[1][1]
-            else:
-                mod_id = 0
-                param1 = 0
-                param2 = 0
-            tmp = np.array([mod_id, param1, param2]).reshape((1, 3))
-            ret.append(tmp)
-        return ret, len(applied_modules)
-
-    def predict_same_size_mod(self, X, mod_id):
-        p = []
-        mod = {}
-        for i in range(1, 9):
-            mod["mod{}".format(i)] = []
-        for elm in X:
-            p.append(elm[0])
-
-            for i in range(1, 9):
-                mod["mod{}".format(i)].append(elm[i])
-        params = []
-        p = tf.convert_to_tensor(np.array(p).reshape(len(X), 32))
-        params.append(p)
-        for i in range(1, 9):
-            params.append(tf.convert_to_tensor(np.array(mod["mod{}".format(i)]).reshape(len(X), 3)))
-        del mod
-        self.disable_layers(1, mod_id)
-        self.disable_layers(mod_id + 1, 8)
-        ret = self.model.predict(params)
-        self.enable_layers()
-        params = []
-        for elm in ret:
-            params.append(elm)
-        return params
-
     def predict(self, X):
-        if len(X) == 3:
-            return self.model.predict(self.sub_pred(X)[0])
-        if X is None:
-            return None
-        Xes = {}
-        for i in range(1, 9):
-            Xes[i] = []
+        X_P30, MOD1, MOD2, MOD3, MOD4, MOD5, MOD6, MOD7, MOD8, _ = process_data(X)
 
-        for i in range(1, len(X)):
-            vec, mod_size = self.sub_pred(X[i])
-            Xes[mod_size].append((i, vec))
-        ret = np.empty((len(X), 32))
-        print("Done ordering")
-        for i in range(1, 9):
-            if len(Xes[i]) > 0:
-                Real_index, X_i = list(zip(*Xes[i]))
-                tmp_res = self.predict_same_size_mod(X_i, i)
-                assert (len(tmp_res) == len(Real_index))
-                for j in range(0, len(Real_index)):
-                    ret[Real_index[j]] = tmp_res[j]
-        return ret
+        return self.model.predict(
+            x=
+            {
+                "R30_input_1": np.array(X_P30).reshape(len(X_P30), 32),
+                "module_params1": np.array(MOD1).reshape((len(MOD1), 3)),
+                "module_params2": np.array(MOD2).reshape((len(MOD1), 3)),
+                "module_params3": np.array(MOD3).reshape((len(MOD1), 3)),
+                "module_params4": np.array(MOD4).reshape((len(MOD1), 3)),
+                "module_params5": np.array(MOD5).reshape((len(MOD1), 3)),
+                "module_params6": np.array(MOD6).reshape((len(MOD1), 3)),
+                "module_params7": np.array(MOD7).reshape((len(MOD1), 3)),
+                "module_params8": np.array(MOD8).reshape((len(MOD1), 3))
+            }
+        )
+
+    def evaluate(self, X, Y):
+        X_P30, MOD1, MOD2, MOD3, MOD4, MOD5, MOD6, MOD7, MOD8, _ = process_data(X)
+
+        self.model.evaluate(
+            x=
+            {
+                "R30_input_1": np.array(X_P30).reshape(len(X_P30), 32),
+                "module_params1": np.array(MOD1).reshape((len(MOD1), 3)),
+                "module_params2": np.array(MOD2).reshape((len(MOD1), 3)),
+                "module_params3": np.array(MOD3).reshape((len(MOD1), 3)),
+                "module_params4": np.array(MOD4).reshape((len(MOD1), 3)),
+                "module_params5": np.array(MOD5).reshape((len(MOD1), 3)),
+                "module_params6": np.array(MOD6).reshape((len(MOD1), 3)),
+                "module_params7": np.array(MOD7).reshape((len(MOD1), 3)),
+                "module_params8": np.array(MOD8).reshape((len(MOD1), 3))
+            },
+            y=np.array(Y).reshape((len(Y), 32))
+        )
 
     def fit(self, X_train, Y_train):
-        # X_trains is splitted into an array called X_train_same_mod_size (delete all elms where R32 = np.zeros(32))
-        # The first element of this array contains all X_train element that has one module applied
-        # The second element of this array contains all X_train element that has two modules applied etc
-        # Same goes with Y_train
-        X_P30 = [[], [], [], [], [], [], [], []]
-        MOD1 = [[], [], [], [], [], [], [], []]
-        MOD2 = [[], [], [], [], [], [], [], []]
-        MOD3 = [[], [], [], [], [], [], [], []]
-        MOD4 = [[], [], [], [], [], [], [], []]
-        MOD5 = [[], [], [], [], [], [], [], []]
-        MOD6 = [[], [], [], [], [], [], [], []]
-        MOD7 = [[], [], [], [], [], [], [], []]
-        MOD8 = [[], [], [], [], [], [], [], []]
-
-        Y_P30 = [[], [], [], [], [], [], [], []]
-
-        for i in range(0, len(X_train)):
-            example_train = X_train[i]
-            applied_modules = example_train[0]
-            p_in_32 = example_train[1]
-            if np.all(p_in_32 == 0):
-                continue
-
-            length = len(applied_modules)
-            X_P30[length - 1].append(np.array(p_in_32).reshape((1, 32)))
-            Y_P30[length - 1].append(np.array(Y_train[i]).reshape((1, 32)))
-
-            for k in range(0, 8):
-                if k < length:
-                    mod = applied_modules[k]
-                    if mod[0] == 'EDFA':
-                        mod_id = 1
-                    else:
-                        mod_id = -1
-                    param1 = mod[1][0]
-                    param2 = mod[1][1]
-                else:
-                    mod_id = 0
-                    param1 = 0
-                    param2 = 0
-                tmp = np.array([mod_id, param1, param2]).reshape((1, 3))
-                if k == 0:
-                    MOD1[length - 1].append(tmp)
-                elif k == 1:
-                    MOD2[length - 1].append(tmp)
-                elif k == 2:
-                    MOD3[length - 1].append(tmp)
-                elif k == 3:
-                    MOD4[length - 1].append(tmp)
-                elif k == 4:
-                    MOD5[length - 1].append(tmp)
-                elif k == 5:
-                    MOD6[length - 1].append(tmp)
-                elif k == 6:
-                    MOD7[length - 1].append(tmp)
-                elif k == 7:
-                    MOD8[length - 1].append(tmp)
-
-        total_len = 0
-        for i in range(0, 8):
-            total_len += len(X_P30[i])
-        print("Total len : {}".format(total_len))
-        train_dataset_mod = []
-        validation_dataset_mod = []
-        for i in range(0, 8):
-            val_size = int(0.0 * len(X_P30[i]))
-
-            tmp_train = tf.data.Dataset.from_tensor_slices((X_P30[i][val_size:], MOD1[i][val_size:],
-                                                            MOD2[i][val_size:], MOD3[i][val_size:],
-                                                            MOD4[i][val_size:], MOD5[i][val_size:],
-                                                            MOD6[i][val_size:], MOD7[i][val_size:],
-                                                            MOD8[i][val_size:], Y_P30[i][val_size:]
-                                                            ))
-            tmp_valid = tf.data.Dataset.from_tensor_slices((X_P30[i][:val_size], MOD1[i][:val_size],
-                                                            MOD2[i][:val_size], MOD3[i][:val_size],
-                                                            MOD4[i][:val_size], MOD5[i][:val_size],
-                                                            MOD6[i][:val_size], MOD7[i][:val_size],
-                                                            MOD8[i][:val_size], Y_P30[i][:val_size]
-                                                            ))
-            tmp_train = tmp_train.shuffle(buffer_size=1024).batch(self.batch_size)
-            tmp_valid = tmp_valid.batch(self.batch_size)
-            train_dataset_mod.append(tmp_train)
-            validation_dataset_mod.append(tmp_valid)
-        train_acc_metric = keras.metrics.RootMeanSquaredError()
-        val_acc_metric = keras.metrics.RootMeanSquaredError()
-        loss_fn = keras.losses.MeanSquaredError()
-        optimizer = keras.optimizers.Adam(1e-4)
-        # Pre-trainning
-
-        # Trainning
-        for epoch in range(0, self.epochs_num):
-            # print("\nEpoch({})".format(epoch))
-            start_time = time.time()
-            for mods_num in range(0, 8):
-                train_dataset = train_dataset_mod[mods_num]
-                val_dataset = validation_dataset_mod[mods_num]
-                self.disable_layers(mods_num + 2)
-                for step, (xp30_bt, mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, yp30_bt) in enumerate(
-                        train_dataset):
-
-                    with tf.GradientTape() as tape:
-                        logits = self.model([xp30_bt, mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8], training=True)
-
-                        loss_value = loss_fn(yp30_bt, logits)
-                    grads = tape.gradient(loss_value, self.model.trainable_weights)
-                    optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
-                    train_acc_metric.update_state(yp30_bt, logits)
-                    if step % 200 == 0:
-                        pass  # print("Sample : {} -- loss : {}".format((step + 1) * self.batch_size, float(loss_value)))
-
-                for step, (xp30_bt, mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, yp30_bt) in enumerate(
-                        val_dataset):
-                    val_logits = self.model([xp30_bt, mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8], training=False)
-                    val_acc_metric.update_state(yp30_bt, val_logits)
-
-                self.enable_layers()
-            val_acc = val_acc_metric.result()
-            train_acc = train_acc_metric.result()
-            if self.last_ac and self.last_vac:
-                print("Time : {} -- loss : {}".format(epoch, float(loss_value)))
-                print("-------->Acc : {} -- Last diff : {} -- Val acc : {} -- Last val dif : {}".format(
-                    float(train_acc),
-                    float(self.last_ac) - float(
-                        train_acc), float(val_acc),
-                    self.last_vac - float(val_acc)))
-            else:
-                print("Time : {} -- Acc : {} -- Last diff : None".format(time.time() - start_time, float(train_acc)))
-            self.last_ac = float(train_acc)
-            self.last_vac = float(val_acc)
-
-            train_acc_metric.reset_states()
-            val_acc_metric.reset_states()
+        X_P30, MOD1, MOD2, MOD3, MOD4, MOD5, MOD6, MOD7, MOD8, Y_P30 = process_data(X_train, Y_train)
+        print("Total len : {}".format(len(X_P30)))
+        try:
+            self.model.fit(
+                x=
+                {
+                    "R30_input_1": np.array(X_P30).reshape(len(X_P30), 32),
+                    "module_params1": np.array(MOD1).reshape((len(MOD1), 3)),
+                    "module_params2": np.array(MOD2).reshape((len(MOD1), 3)),
+                    "module_params3": np.array(MOD3).reshape((len(MOD1), 3)),
+                    "module_params4": np.array(MOD4).reshape((len(MOD1), 3)),
+                    "module_params5": np.array(MOD5).reshape((len(MOD1), 3)),
+                    "module_params6": np.array(MOD6).reshape((len(MOD1), 3)),
+                    "module_params7": np.array(MOD7).reshape((len(MOD1), 3)),
+                    "module_params8": np.array(MOD8).reshape((len(MOD1), 3))
+                },
+                y=np.array(Y_P30).reshape((len(Y_P30), 32)),
+                epochs=self.epochs_num, batch_size=self.epochs_num
+            )
+        except Exception as inst:
+            print("exception : {}".format(inst))
 
 
 a = Regressor()
@@ -328,13 +219,12 @@ a = Regressor()
 
 X_train, y_train = problem.get_train_data()
 X_test, y_test = problem.get_test_data()
+a.fit(X_train, y_train)
 
-a.fit(X_train[:100], y_train[:100])
-res = a.predict(X_train[:100]).reshape((100, 32))
+a.predict(X_train)
 
-mse = sklearn.metrics.mean_squared_error(res, y_train[:100])
-mse2 = sklearn.metrics.mean_squared_error(res, y_train[:100])
+res = a.predict(X_train)  # .reshape((len(X_train), 32))
+mse = sklearn.metrics.mean_squared_error(res, y_train)
 
 rmse = np.sqrt(mse)
-
 print("rmse : {}".format(rmse))
